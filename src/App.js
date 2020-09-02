4a5c29f2bbf702cfbe2a33048c7c05540abc4765
Matt Neal <findme@mattneal.com.au>
2020-09-03 09:55:59 +1000

    Added tabindex

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <div class="can-focus">
+      <div class="can-focus" tabindex="1">
         Transparent Inside Border
       </div>
     </div>
